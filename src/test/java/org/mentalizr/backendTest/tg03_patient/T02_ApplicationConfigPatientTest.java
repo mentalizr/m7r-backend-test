e7267c15ed4d42e2584164460b1d0fce54e0e8fa
Arthur Picht <picht@arthurpicht.de>
2021-09-09 10:58:03 +0200

    added ApplicationConfigPatientTest

---
+++ src/test/java/org/mentalizr/backendTest/tg03_patient/T02_ApplicationConfigPatientTest.java
@@ -0,0 +1,85 @@
+package org.mentalizr.backendTest.tg03_patient;
+
+import de.arthurpicht.utils.core.strings.Strings;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.mentalizr.backendTest.TestContext;
+import org.mentalizr.backendTest.entities.*;
+import org.mentalizr.client.restService.patient.ApplicationConfigService;
+import org.mentalizr.client.restServiceCaller.exception.RestServiceConnectionException;
+import org.mentalizr.client.restServiceCaller.exception.RestServiceHttpException;
+import org.mentalizr.serviceObjects.frontend.patient.ApplicationConfigPatientSO;
+import org.mentalizr.serviceObjects.frontend.patient.ApplicationConfigPatientSOX;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class T02_ApplicationConfigPatientTest {
+
+    private static TestContext testContext;
+    private static Session session;
+
+    private static Program program;
+    private static Therapist therapist;
+    private static Patient patient;
+
+    @BeforeAll
+    public static void setup() throws TestEntityException {
+        System.out.println("\n>>> Setup >>>");
+
+        testContext = TestContext.getInstance();
+
+        session = new Session(testContext);
+        session.loginAsAdmin();
+
+        program = new ProgramTest(testContext);
+        program.create();
+
+        therapist = new Therapist01(testContext);
+        therapist.create();
+
+        patient = new Patient01(testContext);
+        patient.create(therapist.getId(), program.getProgramId());
+
+        session.logout();
+
+        session.loginAsUser(patient.getUsername(), patient.getPassword());
+    }
+
+    @AfterAll
+    public static void cleanup() throws TestEntityException {
+        System.out.println("\n>>> Clean-up >>>");
+
+        session.logout();
+        session.loginAsAdmin();
+
+        patient.delete();
+        therapist.delete();
+        program.delete();
+
+        session.logout();
+    }
+
+    @Test
+    void applicationConfig() {
+        System.out.println("\n>>> application config >>>");
+
+        ApplicationConfigPatientSO applicationConfigPatientSO;
+        try {
+            applicationConfigPatientSO = new ApplicationConfigService(testContext.getRestCallContext()).call();
+
+            System.out.println(ApplicationConfigPatientSOX.toJsonWithFormatting(applicationConfigPatientSO));
+
+            // There is no mechanism for binding a special project configuration in case of testing.
+            // We are leaving this with some superficial tests:
+
+            assertTrue(Strings.isSpecified(applicationConfigPatientSO.getLogo()));
+            assertTrue(Strings.isSpecified(applicationConfigPatientSO.getName()));
+
+        } catch (RestServiceHttpException | RestServiceConnectionException e) {
+            fail(e);
+        }
+    }
+
+}
